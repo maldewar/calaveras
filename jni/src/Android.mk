98b513bc6ee0d7558b3a2e664e52de5d481abd7a
maldewar <valdemar@gmail.com>
2013-11-29 15:06:56 -0700

    Long term commit

--- jni/src/Android.mk
+++ jni/src/Android.mk
@@ -4,12 +4,12 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := main
 
-SDL_PATH := ../SDL
-SDL_IMAGE_PATH := ../SDL_image
-SDL_TTF_PATH := ../SDL_ttf
-BOX2D_PATH := ../Box2D
-JSON_CPP_PATH := ../JsonCpp
-LIBROCKET_PATH := ../LibRocket
+SDL_PATH := ../sdl
+SDL_IMAGE_PATH := ../sdl_image
+SDL_TTF_PATH := ../sdl_ttf
+BOX2D_PATH := ../box2d
+JSON_CPP_PATH := ../jsoncpp
+LIBROCKET_PATH := ../librocket
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include \
                     $(LOCAL_PATH)/$(SDL_IMAGE_PATH) \
@@ -23,31 +23,60 @@ LOCAL_SRC_FILES := $(SDL_PATH)/src/main/android/SDL_android_main.cpp \
     Util/CFile.cpp \
     Util/CLog.cpp \
     Util/CMath.cpp \
-    Util/CTexture.cpp \
+    Util/TextureUtil.cpp \
     Util/CText.cpp \
+    Util/SurfaceUtil.cpp \
+    Util/RocketUtil.cpp \
+    Util/PathUtil.cpp \
     Common/FPSManager.cpp \
     Engine/CAnimation.cpp \
-    Engine/CAnimationCatalog.cpp \
+    Engine/AnimationCatalog.cpp \
     Engine/AreaCatalog.cpp \
+    Engine/BgCatalog.cpp \
+    Engine/I18NCatalog.cpp \
     Engine/CSprite.cpp \
+    Engine/Camera.cpp \
     Engine/WorldContactListener.cpp \
     Engine/RocketGlue.cpp \
+    Engine/Gradient.cpp \
     Model/CScene.cpp \
-    Model/CLayer.cpp \
-    Model/CWorldLayer.cpp \
+    Model/ChoiceGraph.cpp \
+    Model/Vector2.cpp \
+    Model/Node.cpp \
+    Model/Layer.cpp \
+    Model/FgLayer.cpp \
+    Model/WorldLayer.cpp \
+    Model/BgLayer.cpp \
     Model/CArea.cpp \
     Model/CElem.cpp \
     Model/CUnit.cpp \
-    Model/CEntry.cpp \
-    Manager/CSceneLoader.cpp \
+    Model/Entry.cpp \
+    Model/Exit.cpp \
+    Manager/ConfigManager.cpp \
+    Manager/SceneLoader.cpp \
     Manager/CBoxMaker.cpp \
-    Renderer/CSceneRenderer.cpp \
+    Renderer/SceneRenderer.cpp \
+    Renderer/CompositeRenderer.cpp \
+    Renderer/ChoiceGraphRenderer.cpp \
     CEvent.cpp \
     CFPS.cpp \
     AppState.cpp \
     AppStateManager.cpp \
     AppStateIntro.cpp \
+    AppStateMain.cpp \
     AppStateGame.cpp \
+    AppStateCinematic.cpp \
+    Event/EventStartOnClick.cpp \
+    Event/EventSlotOnClick.cpp \
+    Event/EventMusicOnClick.cpp \
+    Event/EventSoundOnClick.cpp \
+    Event/EventPushStateOnClick.cpp \
+    Event/EventPopStateOnClick.cpp \
+    Event/EventChangeAppState.cpp \
+    Event/EventChangeAppStateToGame.cpp \
+    Event/EventChangeAppStateToCinematic.cpp \
+    Event/EventPauseGame.cpp \
+    Event/EventSetSettings.cpp \
     CApp_OnInit.cpp \
     CApp_OnEvent.cpp \
     CApp_OnLoop.cpp \
